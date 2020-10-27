cd7f4dae78cd7784ee3e98b97b2d4ef612acfefd
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2020-10-27 09:24:02 -0300

    Browsing between routes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
@@ -21,7 +22,7 @@ const Dashboard: React.FC = () => {
   const [inputError, setInputError] = useState('');
 
   const [repositories, setRepositories] = useState<Repository[]>(() => {
-    const storageRepositories = localStorage.getItem('@GithubRegitpositories');
+    const storageRepositories = localStorage.getItem('@GithubRepositories');
 
     if (storageRepositories) {
       return JSON.parse(storageRepositories);
@@ -73,7 +74,10 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.id} href="teste">
+          <Link
+            key={repository.id}
+            to={`/repositories/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -83,7 +87,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
